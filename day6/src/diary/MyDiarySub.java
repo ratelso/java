4250ed5eefe89b3dfa09b9721d24dcba604e8c83
ratelso <46391680+ratelso@users.noreply.github.com>
2019-01-19 11:34:58 +0900

    Add files via upload

--- day6/src/diary/MyDiarySub.java
+++ day6/src/diary/MyDiarySub.java
@@ -5,6 +5,11 @@ import java.awt.FlowLayout;
 import java.awt.Font;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileReader;
+import java.io.FileWriter;
+import java.sql.Date;
 
 import javax.swing.ImageIcon;
 import javax.swing.JButton;
@@ -13,17 +18,19 @@ import javax.swing.JLabel;
 import javax.swing.JOptionPane;
 import javax.swing.JTextField;
 import javax.swing.JTextArea;
+import javax.swing.SwingConstants;
 
 public class MyDiarySub extends JFrame {
 	private final JButton button = new JButton("로그인");
-	private JTextField text;
-	private JTextField text_1;
+	private JTextField titleText;
+	private JTextField noText;
+	JTextArea contentText = new JTextArea();
 	
 	public MyDiarySub() {
 		getContentPane().setForeground(new Color(0, 128, 128));
 		getContentPane().setBackground(new Color(255, 248, 220));
 		getContentPane().setFont(new Font("Californian FB", Font.BOLD, 20));
-		setSize(500, 1000);
+		setSize(470, 800);
 		setTitle("일기장");
 		getContentPane().setLayout(new FlowLayout(FlowLayout.CENTER, 5, 5));
 		
@@ -32,44 +39,93 @@ public class MyDiarySub extends JFrame {
 		getContentPane().add(lblNewLabel);
 		
 		JLabel lblNewLabel_1 = new JLabel("");
-		lblNewLabel_1.setIcon(new ImageIcon("C:\\Users\\pc\\Desktop\\shj\\day6\\diary.jpg"));
+		lblNewLabel_1.setIcon(new ImageIcon("C:\\Users\\pc\\Desktop\\shj\\day6\\deardiary.jpg"));
 		getContentPane().add(lblNewLabel_1);
 		
-		JLabel lblNewLabel_2 = new JLabel("날짜");
-		getContentPane().add(lblNewLabel_2);
+		JLabel lblNewLabel_4 = new JLabel("번호");
+		lblNewLabel_4.setFont(new Font("나눔스퀘어라운드 Light", Font.PLAIN, 12));
+		lblNewLabel_4.setHorizontalAlignment(SwingConstants.LEFT);
+		getContentPane().add(lblNewLabel_4);
 		
-		text = new JTextField();
-		getContentPane().add(text);
-		text.setColumns(10);
+		noText = new JTextField();
+		getContentPane().add(noText);
+		noText.setColumns(12);
 		
 		JLabel lblNewLabel_3 = new JLabel("제목");
+		lblNewLabel_3.setFont(new Font("나눔스퀘어라운드 Light", Font.PLAIN, 12));
 		getContentPane().add(lblNewLabel_3);
 		
-		text_1 = new JTextField();
-		getContentPane().add(text_1);
-		text_1.setColumns(10);
+		titleText = new JTextField();
+		getContentPane().add(titleText);
+		titleText.setColumns(16);
 		
-		JButton btnNewButton_1 = new JButton("저장");
-		
-		
-		JTextArea textArea = new JTextArea();
-		textArea.setRows(20);
-		textArea.setColumns(60);
-		textArea.setLineWrap(true);
-		textArea.setFont(new Font("바탕체", Font.PLAIN, 13));
-		getContentPane().add(textArea);
-		getContentPane().add(btnNewButton_1);
-		
-		
-		JButton btnNewButton = new JButton("다시하기");
-		btnNewButton.addActionListener(new ActionListener() {
+		JButton saveButton = new JButton("일기저장");
+		saveButton.addActionListener(new ActionListener() {
 			public void actionPerformed(ActionEvent e) {
-				text.setText("");
-				text_1.setText("");
-				textArea.setText("");
+				String noT = noText.getText();
+				String titleT = titleText.getText();
+				String contentT = contentText.getText();
+				try { // 에러 잡을 때
+				FileWriter f = new FileWriter("c:/diary/" + noT + ".txt");
+				f.write(noT + "\n"); //쓰기
+				f.write(titleT + "\n");  
+				f.write(contentT);  
+				f.flush(); // 밀어넣기
+				f.close(); // 닫기
+				noText.setText("");
+				titleText.setText("");
+				contentText.setText("");
+				JOptionPane.showMessageDialog(null, "저장됨");
+				}catch (Exception er) { //에러 잡을 때
+					JOptionPane.showMessageDialog(null, "저장 중 에러 발생");
+					//에러 잡아서 어떻게 처리할 지
+				}
 			}
 		});
-		getContentPane().add(btnNewButton);
+		saveButton.setFont(new Font("나눔스퀘어라운드 Bold", Font.PLAIN, 12));
+		
+		
+		contentText.setRows(10);
+		contentText.setColumns(35);
+		contentText.setLineWrap(true);
+		contentText.setFont(new Font("나눔스퀘어라운드 Light", Font.PLAIN, 13));
+		getContentPane().add(contentText);
+		getContentPane().add(saveButton);
+		
+		
+		JButton readButton = new JButton("일기읽기");
+		readButton.setFont(new Font("나눔스퀘어라운드 Bold", Font.PLAIN, 12));
+		readButton.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				String noT = noText.getText();
+				try{
+				FileReader f2 = new FileReader("c:/diary/" + noT + ".txt");
+				BufferedReader bfr = new BufferedReader(f2); // 통째로 읽어오기
+				String noR = bfr.readLine(); //읽어와서 한 줄 씩 
+				String titleR = bfr.readLine(); //변수명 첫글자는 소문자
+				String contentR = bfr.readLine();
+				noText.setText(noR);
+				titleText.setText(titleR);
+				contentText.setText(contentR);
+				noText.setForeground(Color.RED);
+				JOptionPane.showMessageDialog(null, "읽음");
+				}catch (Exception er2) {
+					JOptionPane.showMessageDialog(null, "읽기에러발생");
+				}
+			}
+		});
+		getContentPane().add(readButton);
+		
+		JButton resetButton = new JButton("다시하기");
+		resetButton.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				noText.setText("");
+				titleText.setText("");
+				contentText.setText("");
+			}
+		});
+		resetButton.setFont(new Font("나눔스퀘어라운드 Bold", Font.PLAIN, 12));
+		getContentPane().add(resetButton);
 		setVisible(true);
 	}
 	
